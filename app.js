676750fcf2380faad656f835de69641fedebbef6
leoguzi <leoguzi@hotmail.com>
2021-10-16 21:04:06 -0300

    Implemented the games routes, created a new file to hold the database connection info

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 import express from "express";
 import cors from "cors";
-import pg from "pg";
-import { categorieSchema } from "./schemas.js";
+import connection from "./database.js";
+
+import { categorieSchema, gameSchema } from "./schemas.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const { Pool } = pg;
-
-const connection = new Pool({
-  user: "bootcamp_role",
-  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
-  host: "localhost",
-  port: 5432,
-  database: "boardcamp",
-});
 
 app.get("/categories", async (req, res) => {
   try {
@@ -30,8 +21,9 @@ app.post("/categories", async (req, res) => {
   if (categorieSchema.validate(req.body).error) {
     return res.sendStatus(400);
   }
-  const { name } = req.body;
 
+  let { name } = req.body;
+  name = name[0].toUpperCase() + name.substr(1);
   try {
     const result = await connection.query(
       "SELECT * FROM categories WHERE name = $1;",
@@ -45,12 +37,79 @@ app.post("/categories", async (req, res) => {
   }
 
   try {
-    await connection.query("INSERT INTO categories (name) VALUES ($1);", [
-      name,
-    ]);
-    res.sendStatus(200);
+    await connection.query(
+      `INSERT INTO categories (name) 
+        VALUES ($1);`,
+      [name]
+    );
+    res.sendStatus(201);
   } catch (error) {
     res.sendStatus(500);
+  }
+});
+
+app.get("/games", async (req, res) => {
+  try {
+    if (req.query.name) {
+      let { name } = req.query;
+      name = name[0].toUpperCase() + name.substr(1);
+      const result = await connection.query(
+        `SELECT games.*, categories.name as "categoryName" 
+          FROM games JOIN categories 
+            ON games."categoryId"=categories.id
+            WHERE games.name LIKE $1;`,
+        [name + "%"]
+      );
+      res.json(result.rows);
+    } else {
+      const result = await connection.query(
+        `SELECT games.*, categories.name as "categoryName" 
+        FROM games JOIN categories 
+          ON games."categoryId"=categories.id;`
+      );
+      res.json(result.rows);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
+
+app.post("/games", async (req, res) => {
+  if (gameSchema.validate(req.body).error) {
+    return res.sendStatus(400);
+  }
+
+  let { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  name = name[0].toUpperCase() + name.substr(1);
+
+  try {
+    let result = await connection.query(
+      "SELECT * FROM categories WHERE id = $1;",
+      [categoryId]
+    );
+    if (result.rows.length === 0) {
+      return res.sendStatus(400);
+    }
+
+    result = await connection.query("SELECT * FROM games WHERE name = $1;", [
+      name,
+    ]);
+
+    if (result.rows.length > 0) {
+      return res.sendStatus(409);
+    }
+
+    await connection.query(
+      `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+          VALUES($1, $2, $3, $4, $5)`,
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+
+    res.sendStatus(201);
+  } catch (erro) {
+    console.log(erro);
+    return res.sendStatus(500);
   }
 });
 
--- schemas.js
+++ schemas.js
@@ -4,4 +4,12 @@ const categorieSchema = joi.object({
   name: joi.string().alphanum().min(3).max(20).required(),
 });
 
-export { categorieSchema };
+const gameSchema = joi.object({
+  name: joi.string().min(2).max(30).required(),
+  image: joi.string().uri(),
+  stockTotal: joi.number().integer().greater(0),
+  categoryId: joi.number().integer(),
+  pricePerDay: joi.number().greater(0),
+});
+
+export { categorieSchema, gameSchema };
